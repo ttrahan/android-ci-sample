ef6c9d8ec2e66be28619a72babe1c5ad25dbb56c
Tom Trahan <tom@shippable.com>
2017-09-20 16:51:23 -0700

    remove unneeded dirs

--- version/openjdk7.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-echo "================ Installing openjdk-7-jdk ================="
-apt-get install -y openjdk-7-jdk
--- version/openjdk8.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-echo "================ Installing openjdk-8-jdk ================="
-add-apt-repository -y ppa:openjdk-r/ppa
-apt-get update
-apt-get install -y openjdk-8-jdk
-add-apt-repository ppa:maarten-fonville/ppa
-apt-get update
-apt-get install icedtea-8-plugin
